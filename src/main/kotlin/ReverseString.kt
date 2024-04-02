e09f3738ce945ae3bfef6b44ce9e7b265e76764f
priya Kumari <priyakum1071@gmail.com>
2024-04-02 12:56:05 +0530

    String

---
+++ src/main/kotlin/ReverseString.kt
@@ -0,0 +1,22 @@
+fun reverseString(input: String): String {
+    val charArray = input.toCharArray()
+    var left = 0
+    var right = charArray.size - 1
+
+    while (left < right) {
+        val temp = charArray[left]
+        charArray[left] = charArray[right]
+        charArray[right] = temp
+        left++
+        right--
+    }
+
+    return String(charArray)
+}
+
+fun main() {
+    val input = "Good Job!"
+    val reversedString = reverseString(input)
+    println("Original String: $input")
+    println("Reversed String: $reversedString")
+}
